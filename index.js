56237ce201afe729367784a0c7b32c108805ce93
t6dsyuxz <gs1alala102@hotmail.com>
2024-05-07 12:22:05 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-function countAndSay(n) {
-  let result = "1";
-  for (let i = 1; i < n; i++) {
-    let temp = "";
-    let count = 1;
-    for (let j = 0; j < result.length; j++) {
-      if (result[j] === result[j + 1]) {
-        count++;
-      } else {
-        temp += count + result[j];
-        count = 1;
-      }
-    }
-    result = temp;
-  }
-  return result;
+function isPalindrome(str) {
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
 }
+
+console.log(isPalindrome("racecar")); // true
