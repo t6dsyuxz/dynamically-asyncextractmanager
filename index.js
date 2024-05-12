83b54c26d89f668d24a6d6a28f8495cf4b932810
t6dsyuxz <gs1alala102@hotmail.com>
2024-05-12 14:48:59 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-function longestCommonPrefix(strs) {
-  if (strs.length === 0) return "";
-  let prefix = strs[0];
-  for (let i = 1; i < strs.length; i++) {
-    let j = 0;
-    while (
-      j < prefix.length &&
-      j < strs[i].length &&
-      prefix.charAt(j) === strs[i].charAt(j)
-    ) {
-      j++;
-    }
-    prefix = prefix.substring(0, j);
-  }
-  return prefix;
-}
+const initializeArrayWithRange = (end, start = 0) =>
+  Array.from({ length: end - start }, (_, i) => i + start);
